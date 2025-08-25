797f8ac4244c594429fd3232b97d11be598350f1
Muskan Gupta <mksgupta.2004@gmail.com>
2025-08-25 12:06:22 +0530

    auth finally completed

--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,6 +1,6 @@
 
 import express from "express";
-import { isAuth } from "../middleware/isAuth.js";
+import isAuth from "../middleware/isAuth.js";
 import { getCurrentUser } from "../controller/userController.js";
 
 const userRouter=express.Router();
